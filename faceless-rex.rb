235bd787cce9b15a3f9e24daf254a9274bd76a03
matt46244 <github@yeahandstuff.net>
2015-06-09 16:42:17 -0400

    Ayy it works.

--- faceless-rex.rb
+++ faceless-rex.rb
@@ -1,3 +1,15 @@
 #!/usr/bin/env ruby
 
-#how long to get a faceless rex courier?
+#how long to get a faceless rex courier
+#
+rex = 0
+count = 0 
+
+until rex > 0 do
+  count+=1
+  if rand(250) == 1
+    rex+=1
+  end
+end
+
+puts "It would have taken you #{count} attempts to get the Faceless Rex courier! That's #{count*2} dollars!"
