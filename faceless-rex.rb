54213375015069623a1b88ba96ef8acbd45b9374
matt46244 <github@yeahandstuff.net>
2015-06-09 23:52:26 -0400

    Min/Max over 100 tries.

--- faceless-rex.rb
+++ faceless-rex.rb
@@ -1,15 +1,29 @@
 #!/usr/bin/env ruby
 
-#how long to get a faceless rex courier
+#how long to get a faceless rex courie
 #
-rex = 0
-count = 0 
+times = 0
+max = 0
+min = 10000
 
+until times > 99 do
+rex = 0
+count = 0
 until rex > 0 do
   count+=1
   if rand(250) == 1
     rex+=1
+    times+=1
+    if count>= max
+      max = count
+    end
+    if count <= min
+      min = count
+    end
   end
 end
+end
 
-puts "It would have taken you #{count} attempts to get the Faceless Rex courier! That's #{count*2} dollars!"
+#puts "It would have taken you #{count} attempts to get the Faceless Rex courier! That's #{count*2} dollars!"
+puts "Min: #{min} $#{min*2}"
+puts "Max: #{max} $#{max*2}"
